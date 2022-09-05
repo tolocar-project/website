127770a957ae49da5a85a7d590de52612a763fbd
Moritz Stückler <moritz@bitbetter.de>
2022-09-05 14:50:30 +0200

    feat: add github action for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tolocar-project.github.io',
+  base: '/website',
+})
