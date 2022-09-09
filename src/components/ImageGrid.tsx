2ac56c85585eaf9101dd49282263338cdeaa4e29
Moritz Stückler <moritz@bitbetter.de>
2022-09-09 20:35:51 +0200

    fix: who we are bg overlap

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -9,7 +9,7 @@ const ImageGrid: React.FC<Props> = ({ className }: Props) => {
   const imgClasses = "w-full h-full object-cover";
   return (
     <div
-      className={`w-full flex flex-col lg:flex-row gap-4 lg:gap-6 lg:h-[342px] overflow-hidden ${
+      className={`w-full flex flex-col lg:flex-row gap-4 lg:gap-6 lg:h-[342px] z-10 ${
         className || ""
       }`}
     >
@@ -28,13 +28,13 @@ const ImageGrid: React.FC<Props> = ({ className }: Props) => {
         </div>
       </div>
       <div className="flex-1 flex flex-col gap-4 lg:gap-6">
-        <div className="flex-1">
+        <div className="flex-1 overflow-hidden">
           <img
             className={imgClasses}
             src="https://picsum.photos/id/1045/1920/1080"
           />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 overflow-hidden">
           <img
             className={imgClasses}
             src="https://picsum.photos/id/1042/1920/1080"
