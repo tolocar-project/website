93227f63b09cf4dbb99e398a2ce1c531566fc513
Moritz Stückler <moritz@bitbetter.de>
2022-10-06 18:42:47 +0200

    feat: add new navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import TolocarLogoSvg from "../assets/tolocar_logo.svg";
+
+interface Props {
+  className?: string;
+  target?: string;
+}
+
+const Navigation: React.FC<Props> = ({ target = "/", className }: Props) => {
+  const Image = <img className={`h-8 sm:h-10`} src={TolocarLogoSvg} />;
+  return (
+    <div className="w-full bg-white h-20 fixed flex items-center justify-center top-0 z-20">
+      <div
+        className={`container-width flex justify-between ${className || ""}`}
+      >
+        {target ? <a href={target}>{Image}</a> : Image}
+        <nav>
+          <ul className="flex gap-2 text-tolo-footer-grey font-medium font-aktiv text-[15px]">
+            <MenuListItem target="/#top">Home</MenuListItem>
+            <MenuListItem target="/#top">Interventions</MenuListItem>
+            <MenuListItem target="/#top">Community</MenuListItem>
+            <MenuListItem target="/#top">Makerspace Academy</MenuListItem>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+interface MenuListItemProps {
+  target: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const MenuListItem: React.FC<MenuListItemProps> = ({
+  target,
+  className,
+  children,
+}: MenuListItemProps) => (
+  <li className={`px-3 py-2 hover:bg-tolo-light-grey hover:text-tolo-dark-grey rounded-md ${className || ""}`}>
+    <a href={target}>{children}</a>
+  </li>
+);
+
+export default Navigation;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,17 +1,17 @@
-
-import TextBlock from '@components/TextBlock'
-import Card from '@components/Card'
-import HeaderLogo from '@components/HeaderLogo'
-import HeroImage from '@components/HeroImage'
-import SideBySide from '@components/SideBySide'
-import CarCard from '@components/CarCard'
-import HeadlineUnderlined from '@components/HeadlineUnderlined'
-import Footer from '@components/Footer'
+import TextBlock from "@components/TextBlock";
+import Card from "@components/Card";
+import HeaderLogo from "@components/HeaderLogo";
+import HeroImage from "@components/HeroImage";
+import SideBySide from "@components/SideBySide";
+import CarCard from "@components/CarCard";
+import HeadlineUnderlined from "@components/HeadlineUnderlined";
+import Footer from "@components/Footer";
 import ImageGrid from "@components/ImageGrid";
 import ImageCard from "@components/ImageCard";
 import CardContainer from "@components/CardContainer";
 import IllustrationBgContainer from "@components/IllustrationBgContainer";
 import ContentSection from "@components/ContentSection";
+import Navigation from "@components/Navigation";
 
 export {
   ImageGrid,
@@ -27,4 +27,5 @@ export {
   HeaderLogo,
   TextBlock,
   Card,
+  Navigation,
 };
