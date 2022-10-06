8303db272099d46c9ed9022fedfb03ff528470b2
Moritz Stückler <moritz@bitbetter.de>
2022-10-06 17:44:44 +0200

    feat: add ts aliases

---
+++ src/components/index.tsx
@@ -0,0 +1,30 @@
+
+import TextBlock from '@components/TextBlock'
+import Card from '@components/Card'
+import HeaderLogo from '@components/HeaderLogo'
+import HeroImage from '@components/HeroImage'
+import SideBySide from '@components/SideBySide'
+import CarCard from '@components/CarCard'
+import HeadlineUnderlined from '@components/HeadlineUnderlined'
+import Footer from '@components/Footer'
+import ImageGrid from "@components/ImageGrid";
+import ImageCard from "@components/ImageCard";
+import CardContainer from "@components/CardContainer";
+import IllustrationBgContainer from "@components/IllustrationBgContainer";
+import ContentSection from "@components/ContentSection";
+
+export {
+  ImageGrid,
+  ImageCard,
+  CardContainer,
+  IllustrationBgContainer,
+  ContentSection,
+  HeadlineUnderlined,
+  Footer,
+  CarCard,
+  SideBySide,
+  HeroImage,
+  HeaderLogo,
+  TextBlock,
+  Card,
+};
