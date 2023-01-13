2e166711b9121202bcca8bc452bff1530adb02a5
Agustina <agus.carrion@icloud.com>
2023-01-13 14:33:54 +0100

    feat: community section

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -23,3 +23,4 @@ export { default as NewsItem } from "@components/NewsItem";
 export { default as CommunityCard } from "@components/CommunityCard";
 export { default as ButtonLink } from "@components/ButtonLink";
 export { default as ImpactListItem } from "@components/ImpactListItem";
+export { default as CommunityCard } from "@components/CommunityCard";
