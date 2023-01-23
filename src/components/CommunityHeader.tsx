ce9f346c441ae69cbd22a3fcb7d469a3aa63c79e
Agustina <agus.carrion@icloud.com>
2023-01-23 13:55:59 +0100

    feat: update background in community header

--- src/components/CommunityHeader.tsx
+++ src/components/CommunityHeader.tsx
@@ -13,16 +13,12 @@ const CommunityHeader: React.FC<Props> = ({
   text,
 }: Props) => {
   return (
-    <div
-      className={`w-full lg:py-16 background-community background-position-2 lg:background-position-1 mt-14 sm:mt-20 ${
-        className || ""
-      }`}
-    >
-      <div className="container-width">
+    <div className={`w-full bg-neutral-50 lg:pt-16  mt-14 sm:mt-20 ${className || ""}`}>
+      <div className="container-width background-community background-position-2 lg:background-position-1">
         <HeadlineUnderlined large variant={7} className="mt-8">
           {title}
         </HeadlineUnderlined>
-        <div className="flex justify-center py-8 lg:w-[60%] h-full items-start text-lg text-neutral-500">
+        <div className="flex justify-center pt-8 pb-16 lg:pb-24 lg:w-[60%] h-full items-start text-lg text-neutral-500">
           {text}
         </div>
       </div>
