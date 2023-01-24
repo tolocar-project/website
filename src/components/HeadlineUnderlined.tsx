b1ccf41f1f1e57102ae361919fe3323c701f76d9
Tina <109415127+Agustina-Carrion@users.noreply.github.com>
2023-01-24 12:35:46 +0100

    feat: update homepage (#63)
    Co-authored-by: Moritz Stückler <moritz@bitbetter.de>

--- src/components/HeadlineUnderlined.tsx
+++ src/components/HeadlineUnderlined.tsx
@@ -6,6 +6,8 @@ import Underline4Svg from "@assets/tolocar_underline_4.svg";
 import Underline5Svg from "@assets/tolocar_underline_5.svg";
 import Underline6Svg from "@assets/tolocar_underline_6.svg";
 import Underline7Svg from "@assets/tolocar_underline_7.svg";
+import Underline8Svg from "@assets/tolocar_underline_8.svg";
+import CircularUnderlineSvg from "@assets/tolocar_circular_underline.svg";
 
 interface Props {
   children: React.ReactNode;
@@ -22,8 +24,14 @@ const underlineSvgAndClassesMapping = [
   { src: Underline3Svg, classes: "bottom-0" },
   { src: Underline4Svg, classes: "-bottom-4" },
   { src: Underline5Svg, classes: "-bottom-1" },
-  { src: Underline6Svg, classes: "-bottom-1" },
+  { src: Underline6Svg, classes: "bottom-0" },
   { src: Underline7Svg, classes: "bottom-0" },
+  { src: Underline8Svg, classes: "bottom-0" },
+  {
+    src: CircularUnderlineSvg,
+    classes:
+      "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-x-125 scale-y-105 lg:scale-100",
+  },
 ];
 
 const HeadlineUnderlined: React.FC<Props> = ({
@@ -40,8 +48,8 @@ const HeadlineUnderlined: React.FC<Props> = ({
         id={id}
         className={`${
           light ? "text-white" : "text-tolo-black"
-        } relative z-10 font-semibold leading-none font-aktiv ${
-          large ? "text-5xl" : "text-2xl"
+        } relative z-10 font-semibold font-aktiv ${
+          large ? "text-5xl leading-[3.25rem]" : "sm:text-2xl text-lg"
         }`}
       >
         <img
