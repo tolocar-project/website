bda43078315fe79cc3edfb680302e20d41ba2c64
Agustina <agus.carrion@icloud.com>
2023-01-27 16:00:38 +0100

    feat: interventions index subpage

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -30,3 +30,5 @@ export { default as InterventionImageCarousel } from "@components/InterventionIm
 export { default as InterventionPreviewCard } from "@components/InterventionPreviewCard";
 export { default as InterventionSingleViewHeading } from "@components/InterventionSingleViewHeading";
 export { default as Tag } from "@components/Tag";
+export { default as InterventionsHeader } from "@components/InterventionsHeader";
+export { default as InterventionsListItem } from "@components/InterventionsListItem";
