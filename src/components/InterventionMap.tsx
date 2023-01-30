6fee9d8cca89b1837327482442b644e13d7eabc8
Agustina <agus.carrion@icloud.com>
2023-01-30 11:57:15 +0100

    feat: add styling to interventions subpage

--- src/components/InterventionMap.tsx
+++ src/components/InterventionMap.tsx
@@ -5,19 +5,20 @@ import Map, { Marker, NavigationControl } from "react-map-gl";
 
 const InterventionMap = ({
   interventions,
-  token
+  token,
 }: {
-  interventions: MDXInstance<IInterventionFrontmatter>[]; token: string;
+  interventions: MDXInstance<IInterventionFrontmatter>[];
+  token: string;
 }) => {
   return (
     <Map
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
+        longitude: 9.99368,
+        latitude: 53.55108,
+        zoom: 10,
       }}
       style={{ width: "100%", height: 400 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapStyle="mapbox://styles/mapbox/streets-v12"
       mapboxAccessToken={token}
     >
       {interventions.map((intervention, index) => {
@@ -27,11 +28,12 @@ const InterventionMap = ({
             latitude={intervention.frontmatter?.locationLngLat[1]}
             longitude={intervention.frontmatter?.locationLngLat[0]}
             anchor="bottom"
+            color="#009664"
           />
         ) : null;
       })}
       <NavigationControl
-        style={{ borderRadius: "0px" }}
+        style={{ borderRadius: "0px", backgroundColor: "#FFFFFF", color:"#131313" }}
         position="top-right"
         showCompass={false}
       />
--- src/components/InterventionsListItem.tsx
+++ src/components/InterventionsListItem.tsx
@@ -22,47 +22,48 @@ const InterventionsListItem: React.FC<Props> = ({
   href,
 }: Props) => {
   return (
-    <div
-      className={`flex flex-col my-20 lg:flex-row flex-1 overflow-hidden z-10 gap-8 ${
-        className || ""
-      }`}
-    >
-      <div className="w-full h-64 relative">
-        <a href={href}>
-          <img
-            className="w-full h-full object-cover"
-            src={image}
-            alt="Intervention Photo"
-          />
-          <ArrowIcon className="absolute bottom-4 right-4 z-10 text-white h-6 w-6" />
-        </a>
+    <div className={`flex gap-[42px] ${className || ""}`}>
+      <div className="border-l-2 border-neutral-200 relative">
+        <div className="absolute rounded-full bg-tolo-green left-1/2 -translate-x-1/2 top-20 h-5 w-5" />
       </div>
-      <div className="flex flex-col gap-y-6">
-        {tags.length && (
-          <div className="flex justify-start">
-            {tags.map((tag, index) => (
-              <Tag key={index} title={tag} />
-            ))}
-          </div>
-        )}
-        <div className="flex flex-col gap-y-3">
-          <h3 className="font-semibold text-2xl leading-7 uppercase">
-            <span className="pr-3 font-aktiv">{title}</span>
-          </h3>
-          <p className="text-neutral-500 text-base lg:text-lg leading-6 font-medium">
-            {subtitle}
-            <span className="text-tolo-green font-normal">
-              <a href={href}>Read more</a>
-            </span>
-          </p>
+      <div className="flex flex-col my-20 lg:flex-row flex-1 overflow-hidden z-10 gap-8">
+        <div className="w-full h-64 relative">
+          <a href={href}>
+            <img
+              className="w-full h-full object-cover"
+              src={image}
+              alt="Intervention Photo"
+            />
+            <ArrowIcon className="absolute bottom-4 right-4 z-10 text-white h-6 w-6" />
+          </a>
         </div>
-        <div className="flex gap-x-3 items-center text-neutral-500 font-medium">
-          <div className=" text-base leading-6">{date}</div>
-          <DotIcon className="h-1 w-1" />
-          <span className="text-lg leading-[18px] inline-flex items-center gap-3">
-            <LocationIcon className="h-5 w-[15px]" />
-            {location}
-          </span>
+        <div className="flex flex-col gap-y-6">
+          {tags.length && (
+            <div className="flex justify-start">
+              {tags.map((tag, index) => (
+                <Tag key={index} title={tag} />
+              ))}
+            </div>
+          )}
+          <div className="flex flex-col gap-y-3">
+            <h3 className="font-semibold text-2xl leading-7 uppercase">
+              <span className="pr-3 font-aktiv">{title}</span>
+            </h3>
+            <p className="text-neutral-500 text-base lg:text-lg leading-6 font-medium">
+              {subtitle}
+              <span className="text-tolo-green font-normal">
+                <a href={href}>Read more</a>
+              </span>
+            </p>
+          </div>
+          <div className="flex gap-x-3 items-center text-neutral-500 font-medium">
+            <div className=" text-base leading-6">{date}</div>
+            <DotIcon className="h-1 w-1" />
+            <span className="text-lg leading-[18px] inline-flex items-center gap-3">
+              <LocationIcon className="h-5 w-[15px]" />
+              {location}
+            </span>
+          </div>
         </div>
       </div>
     </div>
