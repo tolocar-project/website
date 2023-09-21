c4ce4de07460e3875aea85eb60f674a27d4eeb6f
Moritz Stückler <moritz@bitbetter.de>
2023-09-21 22:15:17 +0200

    fix: menu

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -5,9 +5,9 @@ interface Props {
   className?: string;
 }
 
-const HeroImage: React.FC<Props> = ({ className }: Props) => {
+const HeroImage: React.FC<Props> = ({ className }) => {
   return (
-    <div className="container-width mt-20 md:mt-32">
+    <div className="container-width mt-20 md:mt-32" id="top">
       <div
         className={`grayscale flex p-8 bg-center justify-center w-full h-full items-center bg-[url('/src/assets/Header.jpg')] ${
           className || ""
