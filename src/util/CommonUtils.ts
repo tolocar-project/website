3f61ce1134d3e3fb42dc92840a88ef922ab0f196
Moritz Stückler <moritz@bitbetter.de>
2023-10-17 16:55:21 +0200

    fix: link logic

--- src/util/CommonUtils.ts
+++ src/util/CommonUtils.ts
@@ -2,13 +2,15 @@ const CommonUtils = {
   /**
    * Return the base URL of the current page with or without trailing slash depending on the given parameter
    */
-  getBaseUrl: (removeTrailingSlash?: boolean): string => {
+  getBaseUrl: (trailingSlash: boolean = false): string => {
     const baseUrl = import.meta.env.BASE_URL;
-    if (removeTrailingSlash) {
-      return baseUrl.endsWith("/") ? baseUrl.slice(0, -1) : baseUrl;
-    }
+    const endsWithSlash = baseUrl.endsWith("/");
+    // Add slash
+    if (!endsWithSlash && trailingSlash) return baseUrl + "/";
+    // Remove slash
+    else if (endsWithSlash && !trailingSlash) return baseUrl.slice(0, -1);
     return baseUrl;
-  }
+  },
 };
 
 export default CommonUtils;
