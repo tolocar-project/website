b3de04e340e264c86ecf2dd8083caae8c0bc0382
Moritz Stückler <moritz@bitbetter.de>
2024-01-15 22:36:41 +0100

    chore: dependency updates jan 2024 (#128)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   site: "https://www.tolocar.org",
+  devToolbar: { enabled: false },
   integrations: [
     mdx({
       rehypePlugins: [[rehypeAutolinkHeadings, { behavior: "append" }]],
